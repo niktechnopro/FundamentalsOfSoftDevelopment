d0f661564fb755c6994e1c173b2e175c889bc201
me <me>
2020-06-29 23:14:14 -0400

    Home Work #5

---
+++ UserMovieRating/MovieRatingsProcessor.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
+
+public class MovieRatingsProcessor {
+	
+	public static List<String> getAlphabeticalMovies(TreeMap<String, PriorityQueue<Integer>> movieRatings) {
+		
+		/* IMPLEMENT THIS METHOD! */
+		
+		return null; // this line is here only so this code will compile if you don't modify it
+	}
+
+	public static List<String> getAlphabeticalMoviesAboveRating(TreeMap<String, PriorityQueue<Integer>> movieRatings, int rating) {
+		
+		/* IMPLEMENT THIS METHOD! */
+		
+		return null; // this line is here only so this code will compile if you don't modify it
+	}
+	
+	public static TreeMap<String, Integer> removeAllRatingsBelow(TreeMap<String, PriorityQueue<Integer>> movieRatings, int rating) {
+		
+		/* IMPLEMENT THIS METHOD! */
+		
+		return null; // this line is here only so this code will compile if you don't modify it
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
